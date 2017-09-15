274dadce773c01d870665b9b98403a8009796c01
nandhini <nandhini31@gmail.com>
2017-09-15 14:06:32 +0100

    Feature tests

--- specs/SearchPlacesSpec.js
+++ specs/SearchPlacesSpec.js
@@ -10,14 +10,4 @@ export default function(spec){
       await spec.exists('SearchPage.TextInput');
     });
   });
-
-  spec.describe('Testing the search result display',function(){
-    spec.it('Can view the search result display',async function(){
-      await spec.press('SearchPage.TextInput');
-      await spec.fillIn('SearchPage.TextInput', 'Costa');
-      await spec.press('SearchPage.TextInput', 'Costa');
-      await spec.pause(1000);
-      await spec.exists('ViewPlace.CostaCoffee');
-    });
-  });
 }
