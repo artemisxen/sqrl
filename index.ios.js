cb17ce8042d6658665e198af7ba63dd01d80080a
Sapphire Mason-Brown <saphmb@gmail.com>
2017-09-20 15:16:51 +0100

    App can find saved locations nearby

--- index.ios.js
+++ index.ios.js
@@ -29,11 +29,14 @@ const firebaseApp = firebase.initializeApp({
       messagingSenderId: MESSAGING_SENDER_ID
     });
 
-const geofireRef = new geofire(firebase.database().ref(APP_ID))
-
 class Sqrl extends Component {
-  state = { loggedIn: null, position: null };
-  watchID = null
+  constructor() {
+    super()
+    this.state = { loggedIn: null, position: null };
+    this.watchID = null
+    this.places = null
+    this.nearby = []
+  }
 
   componentWillMount() {
     this.watchID = navigator.geolocation.watchPosition((position) => {
@@ -42,19 +45,40 @@ class Sqrl extends Component {
         longitude: position.coords.longitude,
       }
       this.setState({position});
-      console.log(this.state.position);
+
+          this.places = firebase.database().ref().orderByChild('latitude').startAt(this.state.position.coords.latitude - 0.001).endAt(this.state.position.coords.latitude + 0.001).on("value", (snapshot) => {
+            var trades = snapshot
+            trades.forEach((child) => {
+
+            if (child.val().latitude < this.state.position.coords.latitude + 0.001 && child.val().latitude > this.state.position.coords.latitude - 0.001) {
+              console.log(child.val().name)
+              this.nearby.push({
+                name: child.val().name,
+                longitude: child.val().longitude,
+                latitude: child.val().latitude,
+                _key: child.key
+              })
+              console.log(this.nearby)
+            }
+          })
+        });
+
     });
 
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         console.log(user.email);
-        console.log(firebaseApp);
+        console.log(this.state.position.coords.latitude);
         this.setState({ loggedIn: true });
       } else {
         this.setState({ loggedIn: false });
       }
       console.log(this.state.loggedIn);
-    });
+      });
+    }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchId);
   }
 
   render() {
